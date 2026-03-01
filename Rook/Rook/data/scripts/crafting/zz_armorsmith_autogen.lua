0d3acf9ff2f07c12db4a3a9ed1109945c66bedc2
Kamil <kamilpolkosnik.doit@gmail.com>
2026-03-01 16:54:28 +0100

    revive book recepit change

--- Rook/Rook/data/scripts/crafting/zz_armorsmith_autogen.lua
+++ Rook/Rook/data/scripts/crafting/zz_armorsmith_autogen.lua
@@ -554,17 +554,17 @@ local shieldOverrides = {
             {id = 3955, count = 15} -- voodoo doll
         }
     }, -- blessed shield
-    [7961] = {
-        cost = 3000000,
-        materials = {
-            {id = 5539, count = 1}, -- shield of dorion
-            {id = 2514, count = 3}, -- mastermind shield
-            {id = 2174, count = 15}, -- strange symbol
-            {id = 2151, count = 15}, -- talon
-            {id = 2245, count = 15}, -- twigs
-            {id = 3955, count = 15} -- voodoo doll
-        }
-    }, -- revive spellbook
+	    [7961] = {
+	        cost = 3000000,
+	        materials = {
+	            {id = 5812, count = 1}, -- lizard knowledge spellbook
+	            {id = 2268, count = 200}, -- sudden death rune
+	            {id = 2273, count = 200}, -- ultimate healing rune
+	            {id = 2174, count = 20}, -- strange symbol
+	            {id = 2151, count = 20}, -- talon
+	            {id = 2245, count = 20} -- twigs
+	        }
+	    }, -- revive spellbook
     [2541] = {cost = 600, materials = {}}, -- bone shield (drop)
     [2511] = {disabled = true}, -- brass shield (drop)
     [2535] = {disabled = true}, -- castle shield (drop)
